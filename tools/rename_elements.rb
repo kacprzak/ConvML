d9d7c07787427db196e96bdfc958598452b47609
Marcin Kacprzak <martin.kacprzak@gmail.com>
2011-12-07 11:32:58 +0000

    skrypt do masowej zmiany nazwy elementów

--- tools/rename_elements.rb
+++ tools/rename_elements.rb
@@ -1,20 +1,34 @@
+# -*- coding: utf-8 -*-
+# Program do zmiany nazw elementów ConvML w plikach tekstowych
+
 require 'nokogiri'
-doc = Nokogiri::XML(File.open("xslt/convml_update_10to12.xslt"))
 
-rename_hash = {}
-renames = doc.xpath("//ren:element", 'ren' => 'http://entertech.com.pl/xslt/rename')
-renames.each {|r| rename_hash[r["from"]] = r["to"] }
+def create_rename_hash
+  doc = Nokogiri::XML(File.open("../xslt/convml_update_10to12.xslt"))
+  rename_hash = {}
+  renames = doc.xpath("//ren:element", 'ren' => 'http://entertech.com.pl/xslt/rename')
+  renames.each {|r| rename_hash[r["from"]] = r["to"] }
+  rename_hash
+end
 
-puts rename_hash
-puts rename_hash.keys.sort {|a,b| b.length <=> a.length}
-#puts rename_hash
-
-File.open('polish.lng', "r") do |f|
-  pol_str = f.read
-  rename_hash.each do |k, v|
-    pol_str.gsub!(k, v)
+def rename_elements(rename_hash, file_in, file_out=file_in + ".out", prefix="=")
+  f_str = ""
+  File.open(file_in, "rb") do |f|
+    f_str = f.read
   end
-  File.open('polish2.lng', "w") do |out|
-    out.puts pol_str
+
+  puts "#{file_in} encoding: #{f_str.encoding}"
+
+  rename_hash.keys.sort.reverse.each do |k|
+    f_str.gsub!(prefix + k, prefix + rename_hash[k])
+  end
+
+  File.open(file_out, "wb") do |f|
+    f.puts f_str
   end
 end
+
+FILES = ["polish.lng", "english.lng"]
+
+rh = create_rename_hash
+FILES.each { |fn| rename_elements(rh, fn) }
